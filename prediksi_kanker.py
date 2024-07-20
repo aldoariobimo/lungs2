3774871498951871e84b734c5a3bf5b46f953327
aldoariobimo <513202000134@mhs.dinus.ac.id>
2024-07-20 17:47:03 +0700

    Update prediksi_kanker.py

--- prediksi_kanker.py
+++ prediksi_kanker.py
@@ -1,31 +1,41 @@
 import streamlit as st
-import pandas as pd
 
-def calculate_lung_cancer_score(age, gender, smoke, asbestos, family):
+def calculate_lung_cancer_score(age, gender, smoke, asbestos, family, alcohol):
     """
-    This function calculates the Gupta Postoperative Respiratory Failure Risk based on the input parameters.
+    This function calculates the Lung Cancer Risk based on the input parameters.
     """
-    # The following is pseudo-code for calculating the score.
-    # Replace with the actual calculation algorithm.
+    # Calculate age score based on given ranges
+    if age < 25:
+        age_score = 1
+    elif 30 <= age <= 50:
+        age_score = 2
+    else:
+        age_score = 3
 
-    score = 0
-    score += age
-    score += gender
-    score += smoke
-    score += asbestos
-    score += family 
+    # Sum up the scores
+    score = age_score + gender + smoke + asbestos + family + alcohol
 
-    return score
+    # Calculate the final score as a percentage
+    final_score = (score / 8) * 100
+    return final_score
 
-st.title('Kanker Paru detektor')
+st.title('Kanker Paru Detektor')
 st.write('Nindya Maritza Sulistyowati E13.2022.00197')
 
 age = st.slider('Age', 0, 100, 30)
-gender = st.selectbox('Gender', [(0, 'Male'), (1, 'Female')])
-smoke = st.selectbox('Smoke', [(0, 'No'), (1, 'Yes')])
-asbestos = st.selectbox('Exposure to asbestos', [(0, 'No'), (1, 'Yes')])
-family = st.selectbox('Family history of lung cancer', [(0, 'No'), (1, 'Yes')])
+gender = st.selectbox('Gender', [('Male', 0), ('Female', 1)])
+smoke = st.selectbox('Smoke', [('No', 0), ('Yes', 1)])
+asbestos = st.selectbox('Exposure to asbestos', [('No', 0), ('Yes', 1)])
+family = st.selectbox('Family history of lung cancer', [('No', 0), ('Yes', 1)])
+alcohol = st.selectbox('Alcohol consumption', [('No', 0), ('Yes', 1)])
 
 if st.button('Calculate Score'):
-    score = calculate_lung_cancer_score(age, gender[0], smoke[0], asbestos[0], family [0])
-    st.write('The Lung Cancer Risk is: ', score)
+    score = calculate_lung_cancer_score(age, gender[1], smoke[1], asbestos[1], family[1], alcohol[1])
+    st.write('The Lung Cancer Risk is: {:.2f}%'.format(score))
+
+    if score < 40:
+        st.write('Low Risk Lung Cancer')
+    elif 40 <= score <= 60:
+        st.write('Medium Risk Lung Cancer')
+    else:
+        st.write('High Risk Lung Cancer')
